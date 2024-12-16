c7fedbae84cdb6c3058675e9758756e960b67ec4
joemandell <jg.mandell24@gmail.com>
2024-12-15 22:33:18 -0600

    working MCTS, also added timer to tester

---
+++ graphmaker.py
@@ -0,0 +1,39 @@
+import game
+import algorithm
+import tqdm
+import matplotlib.pyplot as plt
+
+#define a parameter that you want to change in the test algorithm
+#could be search depth for minimax, iteration count for monte carlo, etc.
+variable = range(1,40)
+
+#setup
+y_values = []
+batchsize = 10
+
+for value in variable:
+    print(value)
+
+    #define your algorithm and benchmark
+    test_algo = algorithm.mcts(value)
+    bench_algo = algorithm.minimax(algorithm.basic_heuristic, 2)
+
+    wins = 0
+    for _ in range(batchsize):
+        result, state = game.play(test_algo, bench_algo)
+        if result == 1:
+            wins += 1
+        print(f'{result}, so far {wins} wins.')
+    wins /= batchsize #convert to a percent winrate
+    y_values.append(wins)
+
+print(y_values)
+
+#adjust label text if you want
+label_text = "winrate of minimax algorithm with different search depths, playing against another minimax with depth = 1"
+label_stats = f'batchsize: {batchsize}  variable range {variable}'
+
+
+plt.plot(range(len(variable)), y_values)
+plt.title(label_text + "\n" + label_stats)
+plt.show()
--- tester.py
+++ tester.py
@@ -1,18 +1,23 @@
 import game
 import algorithm
 import tqdm
-import tqdm
+import time
 
 #runs a hundred games of two algorithms against each other.
 white = 0
 games = 10
+start_time = time.time()
 for i in tqdm.trange(games):
     winner, states = game.play(
-        algorithm.mcts(80), #algorithm for white
+        algorithm.mcts(10), #algorithm for white
         algorithm.minimax(algorithm.basic_heuristic, 3), #algorithm for black
         # algorithm.stochastic
     )
     print(winner)
     if winner == 1: white += 1
+
+end_time = time.time()
+print(f'Elapsed time: {(end_time-start_time)/games}')
+
 print(f'Winrate: {(white*100)//games}%')
 #the winrate i
